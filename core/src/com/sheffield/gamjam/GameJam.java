415b5b20f2a774e1e4c5d3d11b03b18efa3b4cf1
AngusHolder <aholder97@gmail.com>
2015-11-14 16:14:48 +0000

    Changed input handling code

--- core/src/com/sheffield/gamjam/GameJam.java
+++ core/src/com/sheffield/gamjam/GameJam.java
@@ -2,20 +2,22 @@ package com.sheffield.gamjam;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameJam extends Game {
 	SpriteBatch batch;
-	Texture img;
-    Player player;
+    public Player player;
+    InputHandler input;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
         player = new Player();
+        input = new InputHandler(this);
+        Gdx.input.setInputProcessor(input);
 	}
 
 	@Override
@@ -24,8 +26,7 @@ public class GameJam extends Game {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         player.update();
 		batch.begin();
-		//batch.draw(img, 0, 0);
         player.render(batch);
-		//batch.end();
+        batch.end();
 	}
 }
--- core/src/com/sheffield/gamjam/Player.java
+++ core/src/com/sheffield/gamjam/Player.java
@@ -8,14 +8,12 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.Gdx;
 
-/**
- * Created by Angus on 14/11/2015.
- */
 public class Player {
 	Texture image;
     Vector2 pos;
     final int LEFT_BOUND, RIGHT_BOUND, UPPER_BOUND, LOWER_BOUND;
     final int BASE_SPEED = 7;
+    final int HIGH_SPEED = 14;
 
     Player() {
         pos = new Vector2(200, 100);
@@ -31,27 +29,25 @@ public class Player {
 
     public void render(SpriteBatch batch) {
         batch.draw(image, pos.x, pos.y);
-        batch.end();
-        ShapeRenderer renderer = new ShapeRenderer();
     }
 
     public void update() {
     	int speed = BASE_SPEED;
     	if (Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT ))
     	{
-    		speed *= 2;
+    		speed = HIGH_SPEED;
     	}
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            pos.x -= speed;//MathUtils.clamp(pos.x - 6, w / 2, screenW - w / 2);
+            pos.x -= speed;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            pos.x += speed; //= MathUtils.clamp(x + 6, w / 2, screenW - w / 2);
+            pos.x += speed;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            pos.y += speed;//MathUtils.clamp(pos.x - 6, w / 2, screenW - w / 2);
+            pos.y += speed;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            pos.y -= speed; //= MathUtils.clamp(x + 6, w / 2, screenW - w / 2);
+            pos.y -= speed;
         }
 
         collideBounds();
