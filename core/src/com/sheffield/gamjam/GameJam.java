1646df8efa38a51e55f2f45b62b711974fba1701
AngusHolder <aholder97@gmail.com>
2015-11-14 20:18:10 +0000

    New branch

--- core/src/com/sheffield/gamjam/GameJam.java
+++ core/src/com/sheffield/gamjam/GameJam.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -11,29 +12,37 @@ public class GameJam extends Game {
 	SpriteBatch batch;
     public Player player;
     public ArrayList<Bullet> bullets;
+	ArrayList<Explosion> explosions;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
         player = new Player(this);
         bullets = new ArrayList<Bullet>();
+		explosions = new ArrayList<Explosion>();
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
+
         player.update();
-		
+
         for(Bullet b : bullets)
         	b.update();
-        
+
         batch.begin();
-        
+
         for(Bullet b : bullets)
         	b.draw(batch);
         player.render(batch);
         batch.end();
+
+		if (e != null) {
+			if (Gdx.input.isKeyPressed(Input.Keys.R)) e.reset();
+			e.update(1.0f / 60.0f);
+			e.render();
+		}
 	}
 }
