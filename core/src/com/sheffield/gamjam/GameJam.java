191d5e5d3a4d995716a4d30bbd2b2ca624815586
Matheus <mdeaguiargomes1@sheffield.ac.uk>
2015-11-14 15:22:30 +0000

    Added ground stuff.

--- core/src/com/sheffield/gamjam/GameJam.java
+++ core/src/com/sheffield/gamjam/GameJam.java
@@ -9,19 +9,20 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class GameJam extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Ground ground;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		// Creating ground object
+		float[] rgb = {0.5f, 1, 0.5f};
+		ground = new Ground(rgb, 100, 20);
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render () {		
+		Gdx.gl.glClearColor(0.4f, 0.4f, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		ground.draw();
 	}
 }
