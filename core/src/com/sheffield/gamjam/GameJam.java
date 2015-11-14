d94c1321f1f7bbe69e4a5c61b5558989249475f8
Blagoslav Mihaylov <blagoslav.mihaylov@gmail.com>
2015-11-14 14:49:04 +0000

    New classes

--- core/src/com/sheffield/gamjam/GameJam.java
+++ core/src/com/sheffield/gamjam/GameJam.java
@@ -1,12 +1,13 @@
 package com.sheffield.gamjam;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class GameJam extends ApplicationAdapter {
+public class GameJam extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
---
+++ core/src/com/sheffield/gamjam/Player.java
@@ -0,0 +1,16 @@
+package com.sheffield.gamjam;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.math.Vector2;
+
+public class Player {
+	
+	Game game;
+	Gun gun;
+	Vector2 pos;
+	
+	public Player(Game g) 
+	{
+		g = game;
+	}
+}
