4ce81de3dea0d5814d3a096558d8395018ac118c
aca14mm <mmolotkov1@sheffield.ac.uk>
2015-11-14 15:23:15 +0000

    Check

--- desktop/src/com/sheffield/gamjam/desktop/DesktopLauncher.java
+++ desktop/src/com/sheffield/gamjam/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.sheffield.gamjam.GameJam;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 720;
+		config.height = 720;
 		new LwjglApplication(new GameJam(), config);
 	}
 }
