e50a41768399b40bbce26b0a2bbea40e292fd9bb
Matheus <mdeaguiargomes1@sheffield.ac.uk>
2015-11-14 13:14:24 +0000

    First

---
+++ desktop/src/com/sheffield/gamjam/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sheffield.gamjam.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sheffield.gamjam.GameJam;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameJam(), config);
+	}
+}
