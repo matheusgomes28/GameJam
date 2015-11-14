1495f10ba94407bbf9d9954a5b693f48ac596371
Matthew Simpson <mjsimpson1@sheffield.ac.uk>
2015-11-14 20:06:23 +0000

    I messed up with git but I'm back to where I was

--- core/src/com/sheffield/gamjam/GameJam.java
+++ core/src/com/sheffield/gamjam/GameJam.java
@@ -1,31 +1,28 @@
 package com.sheffield.gamjam;
 
 import java.util.List;
-import com.badlogic.gdx.Game;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class GameJam extends Game {
+public class GameJam extends ApplicationAdapter {
 	SpriteBatch batch;
 	List<Building> buildings;
-	public Player player;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		player = new Player();
 		// buildings = Building.setupBuildings();   // <-- include when buildings are wanted or use specific setup
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        player.update();
 		batch.begin();
-		player.render(batch);
 		Building.updateAndDraw(buildings, batch);
 		batch.end();
 	}
